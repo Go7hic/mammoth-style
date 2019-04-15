c59d5d187ecc2ff49ef84591412993ec8f7979da
Go7hic <gtfx0209@qq.com>
2019-04-15 23:08:22 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mammoth-style
 Convert Word documents (.docx files) to HTML (with style)
+
+> This project is inspired by [Mammoth](https://github.com/mwilliamson/mammoth.js)
---
+++ test/options-reader.tests.js
@@ -0,0 +1,47 @@
+var assert = require("assert");
+
+var optionsReader = require("../lib/options-reader");
+var standardOptions = optionsReader._standardOptions;
+var readOptions = optionsReader.readOptions;
+var test = require("./test")(module);
+
+
+test('standard options are used if options is undefined', function() {
+    assert.deepEqual(standardOptions, readOptions(undefined));
+});
+
+test('standard options are used if options is empty', function() {
+    assert.deepEqual(standardOptions, readOptions({}));
+});
+
+test('custom style map as string is prepended to standard style map', function() {
+    var options = readOptions({
+        styleMap: "p.SectionTitle => h2"
+    });
+    assert.deepEqual("p.SectionTitle => h2", options.styleMap[0]);
+    assert.deepEqual(standardOptions.styleMap, options.styleMap.slice(1));
+});
+
+test('custom style map as array is prepended to standard style map', function() {
+    var options = readOptions({
+        styleMap: ["p.SectionTitle => h2"]
+    });
+    assert.deepEqual("p.SectionTitle => h2", options.styleMap[0]);
+    assert.deepEqual(standardOptions.styleMap, options.styleMap.slice(1));
+});
+
+test('lines starting with # in custom style map are ignored', function() {
+    var options = readOptions({
+        styleMap: "# p.SectionTitle => h3\np.SectionTitle => h2"
+    });
+    assert.deepEqual("p.SectionTitle => h2", options.styleMap[0]);
+    assert.deepEqual(standardOptions.styleMap, options.styleMap.slice(1));
+});
+
+test('default style mappings are ignored if includeDefaultStyleMap is false', function() {
+    var options = readOptions({
+        styleMap: "p.SectionTitle => h2",
+        includeDefaultStyleMap: false
+    });
+    assert.deepEqual(["p.SectionTitle => h2"], options.styleMap);
+});
